252f848f1458bafea959ce27808b0fc340d426f3
rishithakkar <rishithakkar48@gmail.com>
2021-12-21 21:09:35 +0530

    feat: create new react app using ts refactor: refactoring default code and put static data for init commit
    feat: create api folder for API and setup basic server

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div>Search box</div>
+      <div>List</div>
+    </div>
+  );
+}
+
+export default App;
